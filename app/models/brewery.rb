2ff2b6ffa8a08a2f0fef487280d58fdbd3450fc3
Derek George <derek68038@gmail.com>
2015-03-21 15:58:48 -0500

    Added validations and associations to style and brewery models.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
   attr_accessible :place, :description
+  
+  validates :text, presence: true
+  validates :description, presence: true
+  
+  has_and_belongs_to_many :beers, join_table: :beers_breweries
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,12 @@
 class Style < ActiveRecord::Base
   attr_accessible :category, :color, :bitterness, :alcohol, :flavor, :related
+  
+  validates :category, presence: true
+  validates :color, presence: true
+  validates :bitterness, presence: true
+  validates :alcohol, presence: true
+  validates :flavor, presence: true
+  validates :related, presence: true
+  
+  has_many :beers
 end
