2ecd4c32a255507877b2d061bdccb303b3cd98cd
Derek George <derek68038@gmail.com>
2015-03-21 14:08:28 -0500

    Created new project folder to convert beer sinatra project to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerToRails::Application.config.secret_token = '8cb858807b4e70b27fca3b118eeaa4e5292ca507630a12f234aae7f43bbd40b0a881f01f4eafadc939b4bd9d21f7a3dca66a20b7d5cd3fbf17a1cc2e74825f4b'
