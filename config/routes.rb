ef8c83559757aef3d201b1075a81028b20581ce2
Derek George <derek68038@gmail.com>
2015-03-21 16:12:55 -0500

    Created restful routes for beers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 BeerToRails::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
+  resources :beers
+  
+  get "home" => 'beers#home', :as => "home"
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
