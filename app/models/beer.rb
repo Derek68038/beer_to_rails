5308c31ec26f6caa822c99d917ce3336eff69eb5
Derek George <derek68038@gmail.com>
2015-03-21 14:29:38 -0500

    Created DB for beers table. Labeled field names

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,3 @@
 class Beer < ActiveRecord::Base
+  attr_accessible :name, :style_id, :color, :ibu, :abv, :review, :date
 end
--- db/migrate/20150321192209_beer_model.rb
+++ db/migrate/20150321192209_beer_model.rb
@@ -1,5 +1,15 @@
 class BeerModel < ActiveRecord::Migration
   def change
-    
+    create_table :beers do |t|
+      t.text :name
+      t.integer :style_id
+      t.text :color
+      t.integer :ibu
+      t.float :abv
+      t.text :review
+      t.text :date
+      
+      t.timestamps
+    end
   end
 end
