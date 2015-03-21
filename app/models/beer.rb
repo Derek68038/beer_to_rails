2dcdf2828df78943d829f13b73b7f5731ad57f6b
Derek George <derek68038@gmail.com>
2015-03-21 15:54:35 -0500

    Added validations and associations to beer model.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,14 @@
 class Beer < ActiveRecord::Base
   attr_accessible :name, :style_id, :color, :ibu, :abv, :review, :date
+  
+  validates :name, presence: true
+  validates :style_id, presence: true
+  validates :color, presence: true
+  validates :ibu, presence: true
+  validates :abv, presence: true
+  validates :review, presence: true
+  validates :date, presence: true
+  
+  belongs_to :style
+  has_and_belongs_to_many :breweries, join_table: :beers_breweries
 end
