f476930e747170ef76d9d780f5466579fd647c9f
Derek George <derek68038@gmail.com>
2015-03-24 10:04:37 -0500

    Added migration for users table

---
+++ db/migrate/20150324150230_users_table.rb
@@ -0,0 +1,10 @@
+class UsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :password
+      
+      t.timestamps
+    end
+  end
+end
